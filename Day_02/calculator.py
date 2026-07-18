6355cb4175564eef41d740c6a4a53ac4f0cea965
JahnaviKrishna Kancharla <jahnavikrishnakancharla@gmail.com>
2026-07-18 14:57:16 +0530

    Add basic calculator functionality in calculator.py

---
+++ Day_02/calculator.py
@@ -0,0 +1,13 @@
+num1=int(input("Enter your first number: "))
+num2=int(input("Enter your second number: "))
+
+print("\n============CALCULATOR===============")
+print("Addition of two numbers                  :",num1+num2)
+print("Subtraction of two numbers               :",num1-num2)
+print("Multiplication of two numbers            :",num1*num2)
+print("Division of two numbers                  :",num1/num2)
+print("Floor Division of two numbers            :",num1//num2)
+print("Modulus of two numbers                   :",num1%num2)
+print("Power of two numbers                     :",num1**num2)
+print("=======================================")
+
