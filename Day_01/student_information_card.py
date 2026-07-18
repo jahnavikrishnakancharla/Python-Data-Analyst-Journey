7e9e643ed68ec6134f5ca8df9afb1d46761a9d3c
JahnaviKrishna Kancharla <jahnavikrishnakancharla@gmail.com>
2026-07-18 14:49:08 +0530

    Add student information card script

---
+++ Day_01/student_information_card.py
@@ -0,0 +1,34 @@
+# ==========================================
+# Day 1 - Student Information Card
+# Author: Jahnavi Krishna
+# ==========================================
+
+name = input("Enter your full name: ")
+age = int(input("Enter age: "))
+gender = input("Enter (Male/Female): ")
+clg_name = input("Enter your college name: ")
+branch = input("Enter branch: ")
+year = int(input("Enter year of studying: "))
+city = input("Enter city: ")
+state = input("Enter state: ")
+cgpa = float(input("Enter CGPA: "))
+email_id = input("Enter your email: ")
+phone_no = input("Enter phone number: ")
+fav_sub = input("Enter favourite subject: ")
+career_goal = input("Enter your career goal: ")
+
+print("\n========== STUDENT INFORMATION ==========")
+print("Name         :", name)
+print("Age          :", age)
+print("Gender       :", gender)
+print("College      :", clg_name)
+print("Branch       :", branch)
+print("Year         :", year)
+print("City         :", city)
+print("State        :", state)
+print("CGPA         :", cgpa)
+print("Email        :", email_id)
+print("Phone Number :", phone_no)
+print("Favourite Subject :", fav_sub)
+print("Career Goal  :", career_goal)
+print("=========================================")
