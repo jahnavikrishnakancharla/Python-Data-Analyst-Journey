6ae8d223e27b476563d0fa6b57c1b0d477e45536
JahnaviKrishna Kancharla <jahnavikrishnakancharla@gmail.com>
2026-08-13 14:16:23 +0530

    Add while loop examples for number printing
    Added four programs demonstrating while loops: printing numbers from 1 to 10, 10 to 1, even numbers up to 20, and odd numbers up to 20.

---
+++ Day_06/while_practice.py
@@ -0,0 +1,32 @@
+# Program 1 - Print 1 to 10
+
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+
+
+# Program 2 - Print 10 to 1
+
+i = 10
+while i >= 1:
+    print(i)
+    i = i - 1
+
+
+# Program 3 - Even Numbers
+
+i = 1
+while i <= 20:
+    if i % 2 == 0:
+        print(i)
+    i = i + 1
+
+
+# Program 4 - Odd Numbers
+
+i = 1
+while i <= 20:
+    if i % 2 != 0:
+        print(i)
+    i = i + 1
