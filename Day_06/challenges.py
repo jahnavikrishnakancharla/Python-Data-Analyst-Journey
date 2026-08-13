c4dd99e0d1b39c4861a0128be41085aa8f68fb1e
JahnaviKrishna Kancharla <jahnavikrishnakancharla@gmail.com>
2026-08-13 14:17:01 +0530

    Create challenges.py with challenge placeholders
    Added a challenges.py file with placeholders for multiple challenges.

---
+++ Day_06/challenges.py
@@ -0,0 +1,21 @@
+# =========================
+# Challenge 1
+# =========================
+
+# your code
+
+
+# =========================
+# Challenge 2
+# =========================
+
+# your code
+
+
+# =========================
+# Challenge 3
+# =========================
+
+# your code
+
+# Continue for the challenges you completed
